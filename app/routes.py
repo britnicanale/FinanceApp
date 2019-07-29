9bb1c479db3f10d766fb5067b375a7ff667c2579
EC2 Default User <ec2-user@ip-172-31-32-218.us-east-2.compute.internal>
2019-07-29 20:02:44 +0000

    Added login stuff

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,29 @@
 import os
 from app import app
-from flask import render_template, session, url_for, request, redirect
+from flask import render_template, session, url_for, request, redirect, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import random
 
 app.config['MONGO_DBNAME'] = 'users' 
 app.config['MONGO_URI'] = 'mongodb+srv://admin:Fdc69t6o4s5Pjx65@cluster0-rqyqd.mongodb.net/users?retryWrites=true&w=majority' 
 mongo = PyMongo(app)
 
 
-@app.route('/login')
+app.secret_key = os.urandom(32)
+
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-    if "username" not in session:
-        return render_template("login.html")
-    return redirect(url_for("home"))
+#    if "userName" not in session:
+#         un = request.form['username']
+#         pw = request.form['password']
+#         uncorrect = un == username                   
+#         pwcorrect = pw == password
+#         if uncorrect and pwcorrect:
+#           session['username'] = un
+#           return render_template("login.html", username = un)
+#         return render_template("error.html")
+    return render_template("login.html") 
+    
     
 @app.route("/logout")
 def logout():
@@ -37,9 +46,27 @@ def welcome():
     return render_template("welcome.html", uname = uname)"""
 
 
+#uname = "fintech"
+#pword = "focus"
 
-@app.route('/')
+@app.route('/', methods  = ["POST", "GET"])
 def home():
+    if "username"not in session:
+        if "uname" not in request.form:
+            return redirect(url_for("login"))
+        un = request.form["uname"]                
+        pw = request.form["pword"]
+        uncorrect = un == uname                   
+        pwcorrect = pw == pword
+        if uncorrect and pwcorrect:               
+            session["username"] = uname     
+            return render_template("home.html", uname = un)
+        flash("Your Credentials Are Incorrect")
+        return render_template("login.html")     
+    return render_template("home.html", uname = uname)
+    
+    """
+    #flash("HELLO WHAT'S GOING ON")
     # connect to mongo.db and access collection known as events
     # find will find all the data
     # store collection as events and as a dictionary
@@ -47,26 +74,59 @@ def home():
     collection = mongo.db.users
     users = collection.find({})
     return render_template('home.html', users=users)
-
-@app.route('/add' ,methods=['GET', 'POST'])
-def add():
+"""
+@app.route('/register/execute', methods=['GET', 'POST'])
+def registerexecute():
     if request.method == 'GET':
-        return "Please enter your user information"
-    else:
+        return redirect(url_for("register"))
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'userName' : request.form['userName']})
+    if existing_user is None:
         collection = mongo.db.users
-        users = collection.find({})
+        #users = collection.find({})
         first = request.form['first']
         last = request.form['last']
         dob = request.form['dob']
         email = request.form['email']
         password = request.form['password']
-        bank = request.form['bank']
-        users.insert({'first': first, 'last': last, 'dob': dob, 'email': email, 'password': password, 'bank': bank})
-        #userId = last.charAt(0).upper() + str(random.randint(0,10)) + str(random.randint(0,10)) + str(random.randint(0,10)) + str(random.randint(0,10)) + str(userCount) 
-        #for x in users:
-            #if x['userId'] == userId:
-            #userId += str(random.randint(0,10))
-            
-# song = collection.find_one
-        
-# CONNECT TO DB, ADD DATA
+        userName = request.form['userName']
+        #userId = collection.find_one({'_id': ObjectId(userName)})
+        collection.insert({'first': first, 'last': last, 'dob': dob, 'email': email, 'password': password, 'userName': userName}) #, 'userId': userId})
+        return redirect(url_for('login'))
+    return redirect(url_for("login"))
+       
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
+
+app.config['MONGO_DBNAME'] = 'income' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:Fdc69t6o4s5Pjx65@cluster0-rqyqd.mongodb.net/income?retryWrites=true&w=majority' 
+@app.route('/addincome', methods = ['GET'])
+def income(): 
+    #if request.method == 'GET':
+       # return "Please use the homepage"
+    #collection = mongo.db.income
+    #income = collection.find({})
+    #income.insert({'amount': request.form['amount'] ,'type': request.form['type'], 'source' : request.form['source'], session['userName']})
+    return render_template("addincome.html")
+
+
+app.config['MONGO_DBNAME'] = 'expenses' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:Fdc69t6o4s5Pjx65@cluster0-rqyqd.mongodb.net/expenses?retryWrites=true&w=majority' 
+@app.route("/addexpense", methods = ['POST', 'GET'])
+def expense():
+    if request.method == 'GET':
+        return "Please login"
+    collection = mongo.db.expenses
+    expenses = collection.find({})
+    expenses.insert({'amount': request.form['amount'], 'item': request.form['item'], 'type': request.form['type']})
+    return render_template("addexpense.html")
+    
+    
+    
+
+    
+
+
+
--- app/static/index.js
+++ app/static/index.js
@@ -1,43 +1,64 @@
 // set the dimensions and margins of the graph
-var width = '100%';
-var height = '100%';
-var margin = 40;
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+var body = document.getElementsByTagName("body")[0]
+console.log(body)
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#my_dataviz")
-  .append("svg")
+function makeChart(){
+  
+  var container = d3.select("#my_dataviz")
+
+  container.select("svg").remove();
+  console.log("CONTAINER")
+  console.log(container)
+
+
+  var width = container.node().getBoundingClientRect().width * .8;
+  var height = width
+
+  console.log("WIDTH")
+  console.log(width)
+
+  var margin = 40;
+  // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+  var radius = Math.min(width, width) / 2 - margin
+
+  // append the svg object to the div called 'my_dataviz'
+  var svg = d3.select("#my_dataviz")
+    .append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-// Create dummy data
-var data = {a: 9, b: 20, c:30, d:8, e:12}
+  // Create dummy data
+  var data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
 
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(data)
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+  // set the color scale
+  var color = d3.scaleOrdinal()
+    .domain(data)
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
 
-// Compute the position of each group on the pie:
-var pie = d3.pie()
-  .value(function(d) {return d.value; })
-var data_ready = pie(d3.entries(data))
+  // Compute the position of each group on the pie:
+  var pie = d3.pie()
+    .value(function(d) { return d.value; })
+  var data_ready = pie(d3.entries(data))
 
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(100)         // This is the size of the donut hole
-    .outerRadius(radius)
-  )
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .attr("stroke", "black")
-  .style("stroke-width", "2px")
-  .style("opacity", 0.7)
+  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+  svg
+    .selectAll('whatever')
+    .data(data_ready)
+    .enter()
+    .append('path')
+    .attr('d', d3.arc()
+      .innerRadius(100) // This is the size of the donut hole
+      .outerRadius(radius)
+    )
+    .attr('fill', function(d) { return (color(d.data.key)) })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
+}
+
+makeChart()
+
+window.addEventListener("resize", makeChart);
